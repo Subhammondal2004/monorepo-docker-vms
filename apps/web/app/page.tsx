f21b54e4c59329fd48d29b00b0c31fcbdb359019
Subham Mondal <subham.mondal.23@aot.edu.in>
2026-04-15 19:13:27 +0530

    added ci/cd pipeline and docker push

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from "db/client";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const users = await prisma.user.findMany();
